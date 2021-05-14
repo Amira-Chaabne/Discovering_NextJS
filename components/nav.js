123c7c848d472021c8d1218a6fea8d52a84b3b05
Your Name <you@example.com>
2021-05-14 20:34:19 +0100

    Toggle berger menu

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,6 @@
 import Link from "next/link"
 
+
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light " width={100}>
@@ -10,14 +11,14 @@ export default function Navbar() {
         className="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
+        data-target="#navbarNav1"
+        aria-controls="navbarNav1"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className="desktop">
         <ul className="navbar-nav">
           <li className="nav-item">
             <Link href="/">
@@ -36,6 +37,55 @@ export default function Navbar() {
           </li>
         </ul>
       </div>
+      <div className="collapse navbar-collapse mobile" id="navbarNav1">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link href="/">
+              <a
+                className="nav-link"
+                data-toggle="collapse"
+                data-target="#navbarNav1"
+              >
+                Home
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/otherStuff/About">
+              <a
+                className="nav-link"
+                data-toggle="collapse"
+                data-target="#navbarNav1"
+              >
+                About the author
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/otherStuff/books">
+              <a
+                className="nav-link"
+                data-toggle="collapse"
+                data-target="#navbarNav1"
+              >
+                Books
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <style jsx>{`
+        @media screen and (min-width: 992px) { 
+          .mobile {
+            display: none !important;
+          }
+        }
+        @media screen and (max-width: 991px) { 
+          .desktop {
+            display: none !important;
+          }
+        }
+      `}</style>
     </nav>
   );
 }
